6a73972722ac8760f89fb35154e3c712c33f3894
muhammad-ashik <ashik.chowdhury79@gmail.com>
2021-08-22 12:53:29 +0600

    Updates

---
+++ precache-manifest.8474b6e398884b75eeeac50e88627eb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ababa822c518b4030254a0851b3281ca",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "d536dda1ec91947d2c77",
+    "url": "/blog/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "acdf6ec734101d983c49",
+    "url": "/blog/static/css/main.c745521e.chunk.css"
+  },
+  {
+    "revision": "d536dda1ec91947d2c77",
+    "url": "/blog/static/js/2.28519b14.chunk.js"
+  },
+  {
+    "revision": "b2e167c884a23672a8510d6182ce03fc",
+    "url": "/blog/static/js/2.28519b14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acdf6ec734101d983c49",
+    "url": "/blog/static/js/main.3d1ab550.chunk.js"
+  },
+  {
+    "revision": "069c3457b4d90d99813a",
+    "url": "/blog/static/js/runtime-main.946ba467.js"
+  }
+]);
